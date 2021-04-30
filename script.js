18e72f11060885d5f5e03dbf3ebb1d36ef734a65
Eduardo de Lucca <eduardo.lucca4@gmail.com>
2021-04-30 12:35:14 -0300

    Movimentos da cobrinha pelo teclado

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let canvas = document.getElementById("snake");
+let canvas = document.getElementById("snake"); 
 let context = canvas.getContext("2d");
 let box = 32;
 let snake = [];
@@ -6,6 +6,7 @@ snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
+let direction = "rigth";
 
 function criarBG(){
     context.fillStyle = "lightgreen";
@@ -19,5 +20,40 @@ function criarCobrinha(){
     }
 }
 
-criarBG();
-criarCobrinha();
+document.addEventListener('keydown', update);
+
+function update(event){
+    if(event.keyCode == 37 && direction != "right") direction = "left";
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "rigth";
+    if(event.keyCode == 40 && direction != "up") direction = "down";
+}
+
+function iniciarJogo(){
+    if(snake[0].x > 15 * box && direction == "rigth") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
+    criarBG();
+    criarCobrinha();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == "rigth") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+}
+
+let jogo = setInterval(iniciarJogo, 100);
